c46a11c49c743d164e169fe6d256e15f94ceb7ed
TheSamLloyd <samlloydj@gmail.com>
2018-05-08 23:43:44 -0400

    took out my API key for nyt

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -9,7 +9,7 @@ export default {
     })
   },
   search: function(query, byear=1800, eyear=3000) {
-    return axios.get(url, {params: {q:query, begin_date:byear+"0101", end_date:eyear+"1231", fl:"pub_date, headline, web_url", "api-key":"4b04c58bc3024b7684263e2bdc7cfde6"}})
+    return axios.get(url, {params: {q:query, begin_date:byear+"0101", end_date:eyear+"1231", fl:"pub_date, headline, web_url", "api-key":process.env.nytkey}})
   },
   save: function(article) {
     console.log("saving article!!")
